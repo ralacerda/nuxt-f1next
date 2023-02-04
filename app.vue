e03d66fbec4364fe95be35ad330c8ef611b8cb1c
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-04 11:25:04 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="app-title">F1Next</h1>
+    <div class="round-info">Round 6 of 23</div>
+    <h2 class="round-name">Emilia Romagna Grand Pix</h2>
+    <div class="card">
+      <div class="info-title">
+        <span class="session">First Practice</span>
+        <span class="relative">yesterday</span>
+      </div>
+      <div class="info-datetime">
+        <span class="time">11:30</span>
+        <span class="date">May 19</span>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  routeRules: {
+    "/api/**": {
+      // 2 hour max age, 30 minutes
+      cache: { maxAge: 60 * 60 * 2, staleMaxAge: 60 * 30, swr: true },
+    },
+  },
+});
