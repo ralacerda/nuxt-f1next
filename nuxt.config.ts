cf6dd3aecad716a876736f9cbfa539ac61290125
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-06 09:09:33 -0300

    feat: server cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // routeRules: {
-  //   "/api/**": {
-  //     // 2 hour max age, 30 minutes
-  //     cache: { maxAge: 60 * 60 * 2, staleMaxAge: 60 * 30, swr: true },
-  //   },
-  // },
+  routeRules: {
+    "/api/**": {
+      // 2 hour max age, 30 minutes
+      cache: { maxAge: 60 * 60 * 2, staleMaxAge: 60 * 30, swr: true },
+    },
+  },
   css: ["sanitize.css", "@/assets/index.css"],
   modules: ["unplugin-icons/nuxt", "@vueuse/nuxt"],
 });
