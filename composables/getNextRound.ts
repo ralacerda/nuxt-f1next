bb9d43d8ca6b26de4e13a2bf3bee27161e51233d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-20 10:16:07 -0300

    chore: removing extra lines of code

--- composables/getNextRound.ts
+++ composables/getNextRound.ts
@@ -3,14 +3,12 @@ import type { Schedule } from "~~/types";
 export default function getNextRound(events: Schedule | null) {
   // We add 2 more hours to the current time
   const timeToCheck = new Date(Date.now() - 2 * 60 * 60 * 1000);
-  console.log(`time to check ${timeToCheck}`);
 
   if (events) {
     const nextEvent = events.find((event) => {
       const raceDatetime = event.schedule.filter(
         (event) => event.name == "Race"
       )[0].datetime;
-      console.log(`Race starts at ${new Date(raceDatetime)}`);
       return new Date(raceDatetime) >= timeToCheck ? event.round : false;
     });
 
